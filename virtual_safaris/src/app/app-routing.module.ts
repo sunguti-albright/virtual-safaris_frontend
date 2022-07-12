ed2ed9e3543056fe613438a1fd89c01fa81d70e7
albright <sungutialbright@gmail.com>
2022-07-12 21:29:09 +0300

    register app routes

--- virtual_safaris/src/app/app-routing.module.ts
+++ virtual_safaris/src/app/app-routing.module.ts
@@ -2,16 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PaymentComponent } from './payment/payment.component';
-import { RegisterTouristComponent } from './register-tourist/register-tourist.component';
-import{ LoginTouristComponent} from './login-tourist/login-tourist.component';
 import {TouristProfileComponent} from './tourist-profile/tourist-profile.component';
+import {TouristRegisterComponent} from './tourist-register/tourist-register.component';
+import {TouristLoginComponent} from './tourist-login/tourist-login.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'pay', component: PaymentComponent },
-  { path: 'tourist/register', component: RegisterTouristComponent},
-  {path: 'tourist/login', component:LoginTouristComponent},
   { path: 'tourist/profile', component:TouristProfileComponent},
+  { path: 'tourist/register', component:TouristRegisterComponent },
+  { path: 'tourist/login', component:TouristLoginComponent },
 
 ];
 
